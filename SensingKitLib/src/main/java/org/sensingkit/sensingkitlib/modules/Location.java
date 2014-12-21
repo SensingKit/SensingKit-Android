efc702c3c0696362ef7dc4e8b303d692cbe947c4
Minos Katevas <minos.kat@gmail.com>
2014-12-21 21:18:06 +0000

    Add Location sensor module

---
+++ SensingKitLib/src/main/java/org/sensingkit/sensingkitlib/modules/Location.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2014. Queen Mary University of London
+ * Kleomenis Katevas, k.katevas@qmul.ac.uk
+ *
+ * This file is part of SensingKit-Android library.
+ * For more information, please visit http://www.sensingkit.org
+ *
+ * SensingKit-Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SensingKit-Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SensingKit-Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.sensingkit.sensingkitlib.modules;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import org.sensingkit.sensingkitlib.SKException;
+import org.sensingkit.sensingkitlib.SKSensorDataListener;
+import org.sensingkit.sensingkitlib.model.data.DataInterface;
+import org.sensingkit.sensingkitlib.model.data.LocationData;
+
+public class Location extends AbstractGoogleServicesSensorModule implements LocationListener {
+
+    private static final String TAG = "Location";
+
+    public Location(final Context context) throws SKException {
+        super(context, SensorModuleType.ACTIVITY);
+
+        mClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    public boolean startSensing() {
+
+        this.isSensing = true;
+
+        mClient.connect();
+
+        return true;
+    }
+
+    public void stopSensing() {
+
+        unregisterForLocationUpdates();
+        mClient.disconnect();
+
+        this.isSensing = false;
+    }
+
+    @Override
+    protected void serviceConnected()
+    {
+        Log.i(TAG, "GoogleApiClient Connected!");
+
+        registerForLocationUpdates();
+    }
+
+    @Override
+    public void onLocationChanged(android.location.Location location) {
+
+        // Build the data object
+        DataInterface data = new LocationData(location.getTime(), location);
+
+        // CallBack with data as parameter
+        for (SKSensorDataListener callback : callbackList) {
+            callback.onDataReceived(moduleType, data);
+        }
+    }
+
+    private void registerForLocationUpdates() {
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000); // Update location every second
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mClient, locationRequest, this);
+    }
+
+    private void unregisterForLocationUpdates() {
+
+        LocationServices.FusedLocationApi.removeLocationUpdates(mClient, this);
+
+    }
+}
