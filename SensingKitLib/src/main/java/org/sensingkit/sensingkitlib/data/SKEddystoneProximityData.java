d8231b272f1aa69b7f76b349711cf44a0e6189a0
Minos Katevas <minos.kat@gmail.com>
2016-01-12 14:45:20 +0000

    Add missing license in headers, mentioning Ming's details.

--- SensingKitLib/src/main/java/org/sensingkit/sensingkitlib/data/SKEddystoneProximityData.java
+++ SensingKitLib/src/main/java/org/sensingkit/sensingkitlib/data/SKEddystoneProximityData.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright (c) 2015. Queen Mary University of London
+ * Kleomenis Katevas, k.katevas@qmul.ac.uk
+ * Ming-Jiun Huang, ud2601@gmail.com
+ *
+ * This file is part of SensingKit-Android library.
+ * For more information, please visit http://www.sensingkit.org
+ *
+ * SensingKit-Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SensingKit-Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SensingKit-Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.sensingkit.sensingkitlib.data;
 
 import org.sensingkit.sensingkitlib.SKSensorType;
 
 import java.util.Locale;
-
-/**
- * Created by Ming-Jiun Huang on 11/14/15.
- */
 
 public class SKEddystoneProximityData extends SKAbstractData {
 
